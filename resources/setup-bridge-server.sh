032ce77360cd008960d5755faa544e0e719e8c70
Erik Paulsson <epaulsson@duraspace.org>
2014-02-28 09:44:57 -0500

    Updates for creating group 'duraspace' and adding the tomcat user to this group.  Set owner/permissions on the CONTENT_DIR.

--- resources/setup-bridge-server.sh
+++ resources/setup-bridge-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Installs Oracle JDK7, Tomcat6, and deploys the Duracloud Snapshot WAR file in Tomcat.
+# Installs Oracle JDK, Tomcat, and deploys the Duracloud Snapshot WAR file in Tomcat.
 # @author Erik Paulsson
 
 # define some variables
@@ -8,6 +8,7 @@ JAVA_DIR=java-7-oracle
 TOMCAT_NAME=tomcat6
 MVN_REPO_URL=https://m2.duraspace.org/content/repositories
 MVN_REPO_FILE=releases/org/duracloud/snapshot/snapshot/1.0.0/snapshot-1.0.0.war
+CONTENT_DIR=/export/duraspace
 
 # Add the WebUpd8 Java repo
 echo 'deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main' > /etc/apt/sources.list.d/webupd8team-java.list
@@ -29,8 +30,17 @@ sudo ln -s /usr/lib/jvm/$JAVA_DIR /usr/lib/jvm/default-java
 sudo ln -s /usr/lib/jvm/.$JAVA_DIR.jinfo /usr/lib/jvm/.default-java.jinfo
 sudo update-java-alternatives -s default-java
 
-# Install Tomcat6
+# Install Tomcat
 apt-get install $TOMCAT_NAME -y
 
 # Download and deploy the Duracloud / DPN bridge application
 wget -O /var/lib/$TOMCAT_NAME/webapps/snapshot.war $MVN_REPO_URL/$MVN_REPO_FILE
+
+groupadd duraspace
+usermod -a -G duraspace $TOMCAT_NAME
+chown -R root:duraspace $CONTENT_DIR
+chmod -R g+w $CONTENT_DIR
+
+# Post setup options:
+# Setup apache mod_jk to use apache as proxy to tomcat:
+# http://thetechnocratnotebook.blogspot.com/2012/05/installing-tomcat-7-and-apache2-with.html

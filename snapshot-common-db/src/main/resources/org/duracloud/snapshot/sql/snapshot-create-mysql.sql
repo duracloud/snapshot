9384f5925cb1a5762510236eb1c8bc0a5b2015da
Bill Branan <bbranan@duraspace.org>
2014-08-15 13:29:24 -0400

    Updates the mysql script that is actually getting executed (in snapshot-common-db) based on earlier changes. Also moves the creation of the snapshot table to the top to allow defined foreign keys in other tables to resolve properly.

--- snapshot-common-db/src/main/resources/org/duracloud/snapshot/sql/snapshot-create-mysql.sql
+++ snapshot-common-db/src/main/resources/org/duracloud/snapshot/sql/snapshot-create-mysql.sql
@@ -16,8 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `BATCH_JOB_EXECUTION`
+-- Table structure for table `snapshot`
 --
+
+DROP TABLE IF EXISTS `snapshot`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `snapshot` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `modified` datetime DEFAULT NULL,
+  `description` longtext,
+  `end_date` datetime DEFAULT NULL,
+  `name` longtext NOT NULL,
+  `snapshot_date` datetime DEFAULT NULL,
+  `host` varchar(255) DEFAULT NULL,
+  `port` int(11) NOT NULL,
+  `space_id` varchar(255) DEFAULT NULL,
+  `store_id` varchar(255) DEFAULT NULL,
+  `start_date` datetime DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `status_text` longtext,
+  `user_email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `restoration`
@@ -34,38 +56,14 @@ CREATE TABLE `restoration` (
   `space_id` varchar(255) DEFAULT NULL,
   `store_id` varchar(255) DEFAULT NULL,
   `end_date` datetime DEFAULT NULL,
-  `memo` varchar(512) DEFAULT NULL,
   `start_date` datetime DEFAULT NULL,
   `status` varchar(255) DEFAULT NULL,
+  `status_text` longtext,
+  `user_email` varchar(255) DEFAULT NULL,
   `snapshot_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_ejb7a5btov5hyhb0pyvo3yeb7` (`snapshot_id`),
   CONSTRAINT `FK_ejb7a5btov5hyhb0pyvo3yeb7` FOREIGN KEY (`snapshot_id`) REFERENCES `snapshot` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `snapshot`
---
-
-DROP TABLE IF EXISTS `snapshot`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `snapshot` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `modified` datetime DEFAULT NULL,
-  `description` varchar(512) DEFAULT NULL,
-  `end_date` datetime DEFAULT NULL,
-  `name` varchar(256) NOT NULL,
-  `snapshot_date` datetime DEFAULT NULL,
-  `host` varchar(255) DEFAULT NULL,
-  `port` int(11) NOT NULL,
-  `space_id` varchar(255) DEFAULT NULL,
-  `store_id` varchar(255) DEFAULT NULL,
-  `start_date` datetime DEFAULT NULL,
-  `status` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_50uehv3mbloep8okjgxfn2r26` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -79,15 +77,14 @@ DROP TABLE IF EXISTS `snapshot_content_item`;
 CREATE TABLE `snapshot_content_item` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `modified` datetime DEFAULT NULL,
-  `content_id` varchar(1024) NOT NULL,
-  `content_id_hash` varchar(30) NOT NULL,
-  `metadata` varchar(1024) DEFAULT NULL,
+  `content_id` longtext COLLATE utf8_bin NOT NULL,
+  `content_id_hash` varchar(50) COLLATE utf8_bin NOT NULL,
+  `metadata` longtext COLLATE utf8_bin,
   `snapshot_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK_30tots9ry24rjg42xn08egdrl` (`snapshot_id`,`content_id_hash`),
-  KEY `FK_bif6fhum5u975ks9uo9xufbjh` (`snapshot_id`),
   CONSTRAINT `FK_bif6fhum5u975ks9uo9xufbjh` FOREIGN KEY (`snapshot_id`) REFERENCES `snapshot` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -99,4 +96,4 @@ CREATE TABLE `snapshot_content_item` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-07-22 13:24:02
+-- Dump completed on 2014-08-14 10:06:40

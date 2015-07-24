e3768ca2655424983f77915b0c7a070b627adbf6
Daniel Bernstein <dabylon@gmail.com>
2015-07-24 15:51:16 -0700

    Completes https://jira.duraspace.org/browse/DURACHRON-25: Adds basic security to the snapshot webapp.  root.username and root.password system properties can be set in the     JAVA_OPTS (ie -Droot.username=...).  By default the root username and password is root/rpw.

---
+++ snapshot-bridge-webapp/src/main/java/org/duracloud/snapshot/bridge/rest/config/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+/*
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ *     http://duracloud.org/license/
+ */
+package org.duracloud.snapshot.bridge.rest.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Daniel Bernstein Date: Jul 24, 2015
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/**").hasRole("USER").and().httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser(System.getProperty("root.username", "root"))
+            .password(System.getProperty("root.password", "rpw"))
+            .roles("USER");
+    }
+}

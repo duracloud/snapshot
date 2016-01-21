ec392b2b59d4b9428483fb5c1a54104d8115094c
Daniel Bernstein <dabylon@gmail.com>
2016-01-21 14:59:03 -0800

    Resolves issue #7 in https://wiki.duraspace.org/display/DSPINT/Release+3.7.0+Tasks

--- snapshot-common-db/src/main/resources/org/duracloud/snapshot/sql/snapshot-drop-mysql.sql
+++ snapshot-common-db/src/main/resources/org/duracloud/snapshot/sql/snapshot-drop-mysql.sql
@@ -3,4 +3,7 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `snapshot`;
 DROP TABLE IF EXISTS `restoration`;
 DROP TABLE IF EXISTS `snapshot_content_item`;
+DROP TABLE IF EXISTS `snapshot_alternate_ids`;
+DROP TABLE IF EXISTS `snapshot_history`;
+
 SET FOREIGN_KEY_CHECKS=1;

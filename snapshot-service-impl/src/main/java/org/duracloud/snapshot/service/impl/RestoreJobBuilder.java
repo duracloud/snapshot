eb0e51405305840fa0ea8ea31fbc2932e439945b
Daniel Bernstein <dabylon@gmail.com>
2016-07-15 10:57:03 -0700

    Fixes incorrect code comment.

--- snapshot-service-impl/src/main/java/org/duracloud/snapshot/service/impl/RestoreJobBuilder.java
+++ snapshot-service-impl/src/main/java/org/duracloud/snapshot/service/impl/RestoreJobBuilder.java
@@ -307,7 +307,7 @@ public class RestoreJobBuilder implements BatchJobBuilder<Restoration> {
                                            destinationSpaceId,
                                            false,
                                            true,
-                                           1000*1000*1000); // 1GiB chunk size
+                                           1000*1000*1000); // 1GB chunk size
         endpoint.addEndPointListener(new EndPointLogger());
 
         File watchDir =

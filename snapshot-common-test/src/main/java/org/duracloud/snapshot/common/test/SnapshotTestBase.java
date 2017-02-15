317d92682c6bdfac3af2c9727af3bd629bc9cecc
Daniel Bernstein <dabylon@gmail.com>
2017-02-15 14:22:20 -0800

    Adds -Dbridge.log.dir param for setting the bridge log directory.  Unit tests will use java.io.tmpdir by default if no -Dbridge.log.dir java option is set. Additionally, a redundant logback.xml file was removed.

--- snapshot-common-test/src/main/java/org/duracloud/snapshot/common/test/SnapshotTestBase.java
+++ snapshot-common-test/src/main/java/org/duracloud/snapshot/common/test/SnapshotTestBase.java
@@ -13,6 +13,7 @@ import org.easymock.EasyMockRunner;
 import org.easymock.EasyMockSupport;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 /**
@@ -22,7 +23,15 @@ import org.junit.runner.RunWith;
  */
 @RunWith(EasyMockRunner.class)
 public class SnapshotTestBase extends EasyMockSupport{
- 
+
+    @BeforeClass
+    public static void beforeClass(){
+        String bridgeLogPropKey = "bridge.log.dir";
+        File logDir = new File(System.getProperty("java.io.tmpdir") + File.separator + "duracloud-bridge");
+        logDir.mkdirs();
+        System.setProperty(bridgeLogPropKey, logDir.getAbsolutePath());
+    }
+    
     @Before
     public void setup() throws Exception{
     }

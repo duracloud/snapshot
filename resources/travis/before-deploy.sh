f80757bd58bb0b43f14c2119364af709ef6f7ce4
Bill Branan <bbranan@duraspace.org>
2017-08-21 17:18:08 -0400

    Removes unnecessary before-install build step and ensures targetDir is available

--- resources/travis/before-deploy.sh
+++ resources/travis/before-deploy.sh
@@ -8,8 +8,7 @@ if [ "$TRAVIS_BRANCH" = 'master' ] || [ "$TRAVIS_BRANCH" = 'develop' ]; then
     fi
 fi
 
-
-# function that generates a beanstalk zip 
+# function that generates a beanstalk zip
 generateBeanstalkZip ()
 { echo "Generating beanstalk zip"
    zipFile=$1
@@ -22,17 +21,18 @@ generateBeanstalkZip ()
 }
 
 targetDir=$TRAVIS_BUILD_DIR/target
+mkdir -p $targetDir
+
 currentGitCommit=`git rev-parse HEAD`;
 projectVersion=`mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec`
 
 if [ "$TRAVIS_BRANCH" = 'develop' ] || [ "$TRAVIS_BRANCH" = 'master' ] || [ ! -z "$TRAVIS_TAG" ]; then
    echo "Generating beanstalk zip for $projectVersion ${currentGitCommit}..."
    beanstalkFile="duracloud-bridge-beanstalk-v$projectVersion-${currentGitCommit:0:7}.zip"
-   generateBeanstalkZip ${beanstalkFile} 
+   generateBeanstalkZip ${beanstalkFile}
 
    #make a copy of the beanstalk file using fixed name:
    cp $targetDir/${beanstalkFile} $targetDir/duracloud-bridge-beanstalk-latest.zip
-
 fi
 
 if [ ! -z "$TRAVIS_TAG" ]; then
@@ -45,8 +45,8 @@ if [ ! -z "$TRAVIS_TAG" ]; then
     zipFile=duracloud-bridge-${projectVersion}-apidocs.zip
     echo "Zipping javadocs..."
     zip -r ${zipFile} .
-    mv ${zipFile} $targetDir/ 
-    cd $targetDir 
+    mv ${zipFile} $targetDir/
+    cd $targetDir
     rm -rf install site javadoc-bundle-options
 fi
 

1bfbf2f6c623a969ed778987a027e373d45ee470
Bill Branan <bill.branan@lyrasis.org>
2020-03-31 14:22:37 -0400

    Update to DURACLOUD-1235 - Sets DB entities using LONGTEXT to MEDIUMTEXT (#19)

--- resources/sql/duracloud-1235-schema-update.sql
+++ resources/sql/duracloud-1235-schema-update.sql
@@ -2,3 +2,9 @@ alter table snapshot_content_item modify content_id varchar(2000);
 alter table snapshot modify name varchar(2000);
 alter table snapshot_content_item add index sci_content_id_PK (content_id);
 alter table snapshot_content_item add index snapshot_ci_id_PK (snapshot_id);
+
+alter table snapshot_content_item modify metadata mediumtext;
+alter table snapshot modify description mediumtext;
+alter table snapshot modify status_text mediumtext;
+alter table snapshot_history modify history mediumtext;
+alter table restoration modify status_text mediumtext;
